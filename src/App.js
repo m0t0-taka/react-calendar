450f494e2f919c2347c56b92187b71919ca3f3a7
m0t0-taka <tkg.8.mthr@gmail.com>
2022-08-28 13:06:54 +0900

    Appのベースとcomponent作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import "./App.css";
 import { getMonth } from "./util";
+import { CalendarHeader } from "./components/CalendarHeader";
+import { Sidebar } from "./components/Sidebar";
+import { Month } from "./components/Month";
 
 function App() {
   console.table(getMonth(8));
-  return <></>;
+  return (
+    <>
+      <div className="h-screen flex flex-col">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/CalendarHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CalendarHeader = () => {
+  return <div>CalendarHeader</div>;
+};
