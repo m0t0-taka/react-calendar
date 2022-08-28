b5f6ad59967fc3d72c4062c1a65aa512098ccf1b
m0t0-taka <tkg.8.mthr@gmail.com>
2022-08-28 14:22:38 +0900

    当日の日付に色付

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import dayjs from "dayjs";
 
 export const Day = (props) => {
   const { day, rowIdx } = props;
-  console.log(day);
+
+  // 今日の日付を色付けする
+  const getCurrentDayClass = () => {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  };
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {/* 1行目に曜日を表示 */}
         {rowIdx === 0 && <p className="text-sm mt-1">{day.format("ddd")}</p>}
-        <p className={"text-sm p-1 my-1 text-center"}>{day.format("DD")}</p>
+        <p className={`text-sm p-1 my-1 text-center" ${getCurrentDayClass()}`}>
+          {day.format("DD")}
+        </p>
       </header>
     </div>
   );
