627db96746dcaa81146c1f66cd4f0133c5b27cbe
m0t0-taka <tkg.8.mthr@gmail.com>
2022-08-28 21:09:29 +0900

    表示月の切り替え

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import { getMonth } from "./util";
 import { CalendarHeader } from "./components/CalendarHeader";
 import { Sidebar } from "./components/Sidebar";
 import { Month } from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
+
   return (
     <>
       <div className="h-screen flex flex-col">
--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import GlobalContext from "../context/GlobalContext";
 
 export const CalendarHeader = () => {
+  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  const handlePrevMonth = () => {
+    setMonthIndex(monthIndex - 1);
+  };
+  const handelNextMonth = () => {
+    setMonthIndex(monthIndex + 1);
+  };
   return (
     <header className="px-4 py-2 flex items-center">
       <h1 className="mr-10 text-xl text-gray-500 fond-bold">Calendar</h1>
       <button onClick={""} className="border rounded py-2 px-4 mr-5">
         Today
       </button>
-      <button onClick={""}>
+      <button onClick={handlePrevMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           <MdChevronLeft />
         </span>
       </button>
-      <button onClick={""}>
+      <button onClick={handelNextMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           <MdChevronRight />
         </span>
