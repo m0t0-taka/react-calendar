90468416b607de7eb8b2f6c6f75cbef086d1204a
m0t0-taka <tkg.8.mthr@gmail.com>
2022-08-29 08:38:59 +0900

    日付をクリックしたときのモーダル作成

--- src/context/ContextWrapper.js
+++ src/context/ContextWrapper.js
@@ -4,8 +4,20 @@ import dayjs from "dayjs";
 
 const ContextWrapper = (props) => {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [daySelected, setDaySelected] = useState(dayjs());
+  const [showEventModal, setShowEventModal] = useState(false);
+
   return (
-    <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+    <GlobalContext.Provider
+      value={{
+        monthIndex,
+        setMonthIndex,
+        daySelected,
+        setDaySelected,
+        showEventModal,
+        setShowEventModal,
+      }}
+    >
       {props.children}
     </GlobalContext.Provider>
   );
