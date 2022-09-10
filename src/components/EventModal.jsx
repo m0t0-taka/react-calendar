ed50c4270cf0327da871568cbba31cbc668449da
m0t0-taka <tkg.8.mthr@gmail.com>
2022-09-10 16:23:21 +0900

    時間登録のモック作成

--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { MdDeleteForever, MdClose } from "react-icons/md";
+import { IconContext } from "react-icons";
 
 export const EventModal = (props) => {
   const { daySelected, setShowEventModal, selectedEvent, dispatchCalEvent } =
@@ -27,30 +28,36 @@ export const EventModal = (props) => {
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
-        <header className="bg-gray-100 px-4 py-2 flex justify-end">
-          <div className="text-gray-400">
-            {selectedEvent && (
-              <button
-                onClick={() => {
-                  dispatchCalEvent({ type: "delete", payload: selectedEvent });
-                  setShowEventModal(false);
-                }}
-              >
-                <MdDeleteForever />
+        <header className="bg-gray-100 px-4 pt-2 pb-1 flex justify-between">
+          <div>
+            <p className="text-gray-600 text-lg font-medium">
+              {daySelected.format("MMMM DD日 dddd")}
+            </p>
+          </div>
+          <div>
+            <IconContext.Provider value={{ color: "#9ca3af", size: "25px" }}>
+              {selectedEvent && (
+                <button
+                  className="px-3"
+                  onClick={() => {
+                    dispatchCalEvent({
+                      type: "delete",
+                      payload: selectedEvent,
+                    });
+                    setShowEventModal(false);
+                  }}
+                >
+                  <MdDeleteForever />
+                </button>
+              )}
+              <button onClick={() => setShowEventModal(false)}>
+                <MdClose />
               </button>
-            )}
-            <button onClick={() => setShowEventModal(false)}>
-              <MdClose />
-            </button>
+            </IconContext.Provider>
           </div>
         </header>
         <div className="p-3">
           <div className="grid grid-cols-1/5 items-end gap-y-7">
-            <div>
-              <p className="text-gray-600">
-                {daySelected.format("MMMM DD日 dddd")}
-              </p>
-            </div>
             <input
               type="text"
               name="title"
@@ -60,9 +67,45 @@ export const EventModal = (props) => {
               className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
               onChange={(e) => setTitle(e.target.value)}
             />
+            <div className="px-5 py-2 w-40 bg-white rounded-lg text-lg font-medium outline outline-offset-2 outline-1 outline-gray-200">
+              <div className="flex text-gray-600">
+                <select
+                  name="hours"
+                  className="bg-transparent text-xl appearance-none outline-none"
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+                <span className="text-xl mr-3">:</span>
+                <select
+                  name="minutes"
+                  className="bg-transparent text-xl appearance-none outline-none mr-4"
+                >
+                  <option value="0">00</option>
+                  <option value="30">30</option>
+                </select>
+                <select
+                  name="ampm"
+                  className="bg-transparent text-xl appearance-none outline-none"
+                >
+                  <option value="am">AM</option>
+                  <option value="pm">PM</option>
+                </select>
+              </div>
+            </div>
           </div>
         </div>
-        <footer className="flex justify-end p-3">
+        <footer className="flex justify-center p-3">
           <button
             type="submit"
             onClick={handleSubmit}
