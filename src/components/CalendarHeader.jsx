8213f8e89813ed709d4b2d7aa4389d6d42880d95
m0t0-taka <tkg.8.mthr@gmail.com>
2022-09-11 16:49:27 +0900

    タイトル登録画面と遷移ボタンの追加

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
 import ja from "dayjs/locale/ja";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
@@ -19,6 +20,12 @@ export const CalendarHeader = () => {
     // 現在の月を取得
     setMonthIndex(dayjs().month());
   };
+
+  let navigate = useNavigate();
+
+  const handleTitleList = () => {
+    navigate("/title");
+  };
   return (
     <header className="px-4 py-2 flex items-center">
       <h1 className="mr-10 text-xl text-gray-500 fond-bold">月間カレンダー</h1>
@@ -30,16 +37,19 @@ export const CalendarHeader = () => {
           <MdChevronLeft />
         </span>
       </button>
-      {/* <div> */}
       <h2 className="mx-1 text-xl text-gray-500 font-bold w-32">
         {dayjs(new Date(dayjs().year(), monthIndex)).format("YYYY年 MMM")}
       </h2>
-      {/* </div> */}
-
       <button onClick={handelNextMonth}>
         <span className="cursor-pointer text-gray-600 mx-2">
           <MdChevronRight />
         </span>
+      </button>
+      <button
+        onClick={handleTitleList}
+        className="border rounded py-2 px-4 ml-5"
+      >
+        登録タイトル
       </button>
     </header>
   );
